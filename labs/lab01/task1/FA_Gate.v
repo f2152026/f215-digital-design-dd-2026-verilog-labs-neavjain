89537cce5bcc16b97f98c0e286cba2799a589caa
NeavJain <135718395+NeavJain@users.noreply.github.com>
2026-08-27 21:04:27 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -16,11 +16,6 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  or  (cout, pc1, pc2); 
-  and (pc2, cin, ps);
-  xor (sum, cin, ps);
-  and (pc1, a,   b);
-  xor (ps,  a,   b);   
 
   xor #(2) (ps,  a,   b);
   and #(2) (pc1, a,   b);
